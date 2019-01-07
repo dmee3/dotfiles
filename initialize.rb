3ea2e89114a67a9632c4eef93d172f899838495a
Dan <dmeehan@covermymeds.com>
2019-01-07 15:23:05 -0500

    Add support for Pywal

--- initialize.rb
+++ initialize.rb
@@ -29,28 +29,51 @@ def run_cmd_with_status(cmd, description = 'Running command...', indent = '')
   end
 end
 
+#
 # Bundle install
+#
 run_cmd('bundle install', 'Bundle installing...')
 
+#
 # Brew Dependencies
+#
 puts "#{Colors.blue}Brewing formulae...#{Colors.default}"
 brew_list = `brew ls`
 run_cmd_with_status('brew install fzf', 'Installing fzf...', '  ') unless brew_list =~ /fzf/
 run_cmd_with_status('brew install highlight', 'Installing highlight...', '  ') unless brew_list =~ /highlight/
+
+# Window manager
 unless brew_list =~ /chunkwm/
   run_cmd_with_status('brew tap koekeishiya/formulae', 'Tapping chunkwm cask...', '  ')
   run_cmd_with_status('brew install chunkwm', 'Installing chunkwm...', '  ')
 end
 run_cmd_with_status(`brew install skhd`, 'Installing skhd...', '  ') unless brew_list =~ /skhd/
 
+# Needed for pywal
+run_cmd_with_status(`brew install python`, 'Installing Python 3 with pip3...', '  ') unless brew_list =~ /python/
+run_cmd_with_status(`brew install imagemagick`, 'Installing imagemagick...', '  ') unless brew_list =~ /imagemagick/
+
+#
+# Pywal (custom color schemes by background)
+#
+pip_list = `pip3 list`
+run_cmd_with_status(`pip3 install pywal`, "#{Colors.blue}Installing pywal...#{Colors.default}") unless pip_list ~/pywal/
+
+#
 # Config Curator
+#
 run_cmd('bundle exec curate -v', 'Curating...')
 
+#
 # Setup iTerm2 preferences
+#
 run_cmd('cp themes/com.googlecode.iterm2.plist ~/Library/Preferences/com.googlecode.iterm2.plist', 'Copying iTerm2 preferences...')
 run_cmd('defaults read com.googlecode.iterm2', 'deleting iTerm2 cached settings...')
 
+#
 # Set source
+#
 run_cmd('source ~/.bash_profile', 'Sourcing new profile...')
 
 puts "#{Colors.green}Done!  Please quit and restart iTerm2 for new settings to take effect.#{Colors.default}"
+
